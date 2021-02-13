3de1dc2378ac4e50cbc58629cf0bf4e398114ed1
marcsello <punkosdmarcell@rocketmail.com>
2021-02-13 03:47:29 +0100

    Added more admin stuff

--- src/components/AdminLister.vue
+++ src/components/AdminLister.vue
@@ -1,14 +1,27 @@
 <template>
-  <b-row>
+  <b-row class="my-4">
     <b-col>
-
+      <b-card v-if="uploads.length">
+        <b-table striped hover :items="uploads"></b-table>
+      </b-card>
     </b-col>
   </b-row>
 </template>
 
 <script>
 export default {
-  name: "AdminLister"
+  name: "AdminLister",
+  props: {
+    uploads: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  event: [
+      'showItem'
+  ]
 }
 </script>
 
--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,25 +1,73 @@
 <template>
   <div id="admin">
     <b-container>
-      <admin-requester @submit="performRequest"/>
-      <admin-lister/>
+      <admin-keyfield @submit="onSubmit"/>
+      <admin-lister :uploads="uploads" @showItem="onShowItem"/>
     </b-container>
   </div>
 </template>
 
 <script>
 import AdminLister from "@/components/AdminLister";
-import AdminRequester from "@/components/AdminRequester";
+import AdminKeyfield from "@/components/AdminKeyfield";
+import axios from "axios";
 
 export default {
   name: "Admin",
   components: {
-    AdminRequester, AdminLister
+    AdminKeyfield, AdminLister
+  },
+  data() {
+    return {
+      key: null,
+      uploads: []
+    }
   },
   methods: {
-    performRequest() {
+    onSubmit(key) {
+      this.key = key;
+      this.getList()
+    },
+    adminGet(path) {
+      return new Promise((resolve, reject) => {
+        if (!this.key) {
+          return reject();
+        }
 
-    }
+        const config = {
+          baseURL: process.env.VUE_APP_API_LOCATION,
+          headers: {
+            'Authorization': this.key
+          }
+        }
+
+        return axios.get(
+            path, config
+        ).then((response) => {
+
+          return resolve(response.data);
+
+        }).catch((error) => {
+
+          if (error.response.status === 401) {
+            alert("Rossz kulcs!");
+          } else {
+            alert("Error");
+          }
+          return reject();
+        });
+      })
+    },
+    getList() {
+      this.adminGet('/upload').then((data) => {
+        this.uploads = data;
+      }).catch(() => this.uploads = []);
+    },
+    onShowItem(id) {
+      this.adminGet(`/upload/${id}`).then(() => {
+        // TODO
+      }).catch(() => this.uploads = []);
+    },
   }
 }
 </script>
