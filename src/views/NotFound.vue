ade2a09caee687475f24461b3bb674a925edb820
marcsello <punkosdmarcell@rocketmail.com>
2021-02-14 19:35:34 +0100

    Added 404 page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Ez oldal nem létezik</h1>
+    <p>Valószínűleg hibás linket követett, vagy az oldal már nem létezik.</p>
+    <p><b-button href="/" variant="secondary">Főldal</b-button></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound"
+}
+</script>
+
+<style scoped>
+
+</style>
