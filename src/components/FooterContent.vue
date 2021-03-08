cda442d1c60285b7ccd4d0047eef17f30910b0de
marcsello <punkosdmarcell@rocketmail.com>
2021-03-09 00:45:01 +0100

    Aligned privacy policy link to the right

--- src/components/FooterContent.vue
+++ src/components/FooterContent.vue
@@ -3,7 +3,7 @@
     <b-col>
       © 2021 Pünkösd Marcell
     </b-col>
-    <b-col>
+    <b-col class="text-right">
       <b-link v-b-modal.privacy-policy-popup>Adatkezelési tájékoztató</b-link>
     </b-col>
   </b-row>
