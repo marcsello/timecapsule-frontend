314d749ca4fa81f1a8d711820dc7ebe5c7c36555
marcsello <punkosdmarcell@rocketmail.com>
2021-03-13 17:47:56 +0100

    added listing of selected files

--- src/components/MultipleFileSelect.vue
+++ src/components/MultipleFileSelect.vue
@@ -1,52 +1,58 @@
 <template>
-  <div class="uploadBox">
+  <div>
+    <b-list-group>
+      <b-list-group-item
+          v-for="(item,idx) in items"
+          :key="idx"
+          class="d-flex justify-content-between align-items-center"
+          :variant="getListItemVariant(idx)"
+      >
+        <div class>{{ item.name }}
+          <b-badge
+              :variant="item.size > maxSizePerFile ? 'danger' : 'secondary'"
+          >{{ bytesToSize(item.size) }}</b-badge>
+        </div>
+        <b-button size="sm" variant="outline-danger" @click="removeItem(idx)">
+          <b-icon icon="trash"/>
+        </b-button>
+      </b-list-group-item>
 
-    <div>
-
-      <div
+      <b-list-group-item
           class="dropArea"
           @ondragover="onChange"
           v-bind:class="{dropAreaDragging: dragging, dropAreaJumbo: items.length === 0}"
           @dragenter="dragging=true"
           @dragend="dragging=false"
           @dragleave="dragging=false"
+          v-if="items.length < maxItems"
       >
-        <input ref="fileInputField" type="file" id="items" multiple @change="onChange">
+
+
+        <input :accept="accept" ref="fileInputField" type="file" id="items" multiple @change="onChange">
         <h3>{{ uploaderHelpText }}</h3>
         <h4>Vagy kattintson a tallózáshoz</h4>
-      </div>
 
-    </div>
 
+      </b-list-group-item>
+    </b-list-group>
   </div>
 </template>
 
 <script>
 
-/*
-function bytesToSize(bytes) {
-  // http://scratch99.com/web-development/javascript/convert-bytes-to-mb-kb/
-  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-  if (bytes === 0) return 'n/a';
-  let i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
-  if (i === 0) return bytes + ' ' + sizes[i];
-  return (bytes / Math.pow(1024, i)).toFixed(2) + ' ' + sizes[i];
-}
-*/
-
 export default {
   props: {
-    minItems: {
+    maxSizePerFile: {
       type: Number,
-      default: 0
+      default: 10485760 // 10MB
     },
     maxItems: {
       type: Number,
-      default: 30
+      default: 50
     },
     accept: {
       type: String,
-      default: "image/*"
+      default: ""
     }
   },
   data() {
@@ -55,7 +61,6 @@ export default {
       items: []
     }
   },
-
   methods: {
     onChange(e) {
       this.dragging = false;
@@ -64,6 +69,20 @@ export default {
         this.items.push(file);
       });
       this.$refs.fileInputField.value = ""; // reset
+    },
+    bytesToSize(bytes) {
+      // http://scratch99.com/web-development/javascript/convert-bytes-to-mb-kb/
+      const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+      if (bytes === 0) return '0 Bytes';
+      let i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+      if (i === 0) return bytes + ' ' + sizes[i];
+      return (bytes / Math.pow(1024, i)).toFixed(2) + ' ' + sizes[i];
+    },
+    removeItem(idx) {
+      this.items.splice(idx, 1);
+    },
+    getListItemVariant(idx) {
+      return (idx + 1) > this.maxItems ? 'warning' : ''
     }
   },
   computed: {
@@ -82,27 +101,28 @@ export default {
 </script>
 
 <style scoped>
-.uploadBox .dropArea {
+.dropArea {
   /* Needed for the full sized input */
   position: relative;
   width: 100%;
 
-  border: 1px solid #CED4DA;
-  border-radius: 5px;
   text-align: center;
   padding: 2em 0;
 }
 
-.uploadBox .dropAreaJumbo {
+.dropArea:hover {
+  background-color: #fafafa;
+}
+
+.dropAreaJumbo {
   padding: 4em 0;
 }
 
-.uploadBox .dropAreaDragging {
+.dropAreaDragging {
   box-shadow: #CED4DA 0 0 10px;
-  border-color: #0069D9;
 }
 
-.uploadBox .dropArea input {
+.dropArea > input {
   /* so that file input will cover the entire area */
   position: absolute;
   cursor: pointer;
